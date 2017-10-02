1363a53c5b653f84cb95cbe13f8a99565d7547fe
Anton Iun <iunanton@naver.com>
2017-10-02 18:13:40 +0800

    Handle errors

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 "use strict";
 const uuidv4 = require('uuid/v4');
-// uuidv4();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
-//const url = require('url');
 const app = express();
 const bcrypt = require('bcrypt');
 var mongo = require('mongodb').MongoClient;
-var assert = require('assert');
 var url = 'mongodb://mongo:27017/chat';
 
 var port = process.env.PORT || 80;
@@ -22,37 +18,63 @@ app.route('/')
 	// show authorization form
   res.sendFile(__dirname + '/index.html');
 })
-
-app.post('/', function(req, res) {
+.post(function(req, res) {
 	// receive credentials from user
 	console.log("%s Got request for new UUID", Date.now());
+	// check grant_type
+	if (req.body.grant_type !== "password") {
+		console.log("%s error: unsupported_grant_type", Date.now());
+		res.status(400);
+		res.setHeader("Cache-Control", "no-store");
+		res.setHeader("Pragma", "no-cache");
+		res.json({ "error": "unsupported_grant_type" });
+		return;
+	}
+	var username = req.body.username;
+	var password = req.body.password;
+	// console.log("%s %s %s", Date.now(), req.body.username, req.body.password);
 	// connect to db
 	mongo.connect(url, function(err, db) {
 		if (err) {
-			console.error("%s %s: %s",Date.now(), err.name, err.message);
+			console.error("%s %s: %s", Date.now(), err.name, err.message);
+			res.status(400);
+			res.setHeader("Cache-Control", "no-store");
+			res.setHeader("Pragma", "no-cache");
 			res.json({ "error": err.message });
 			return;
 		}
-		db.collection("users").findOne({ "username": req.body.username }, { "password": 1 }, function (err, r) {
+		db.collection("users").findOne({ "username": username }, { "password": 1 }, function (err, r) {
 			if (err) {
-				console.error("%s %s: %s",Date.now(), err.name, err.message);
+				console.error("%s %s: %s", Date.now(), err.name, err.message);
+				res.status(400);
+				res.setHeader("Cache-Control", "no-store");
+				res.setHeader("Pragma", "no-cache");
 				res.json({ "error": err.message });
 				return;
 			}
 			if (!r) {
-				console.log("%s Username or password incorrect", Date.now());
-				res.json({ "error": "Username or password incorrect" });
+				console.log("%s error: invalid_grant", Date.now());
+				res.status(400);
+				res.setHeader("Cache-Control", "no-store");
+				res.setHeader("Pragma", "no-cache");
+				res.json({ "error": "invalid_grant" });
 				return;
 			}
-			bcrypt.compare(req.body.password, r.password, function(err, auth) {
+			bcrypt.compare(password, r.password, function(err, auth) {
 				if (err) {
-					console.error("%s %s: %s",Date.now(), err.name, err.message);
+					console.error("%s %s: %s", Date.now(), err.name, err.message);
+					res.status(400);
+					res.setHeader("Cache-Control", "no-store");
+					res.setHeader("Pragma", "no-cache");
 					res.json({ "error": err.message });
 					return;
 				}
 				if (!auth) {
-					console.log("%s Username or password incorrect", Date.now());
-					res.json({ "error": "Username or password incorrect" });
+					console.log("%s error: invalid_grant", Date.now());
+					res.status(400);
+					res.setHeader("Cache-Control", "no-store");
+					res.setHeader("Pragma", "no-cache");
+					res.json({ "error": "invalid_grant" });
 					return;
 				}
 				// create user's uuid
@@ -61,19 +83,22 @@ app.post('/', function(req, res) {
 				// update token into db
 				db.collection("users").updateOne({ "username": req.body.username }, { $set: { "userUuid" : uuid } }, function (err, r) {
 					if (err) {
-						console.error("%s %s: %s",Date.now(), err.name, err.message);
+						console.error("%s %s: %s", Date.now(), err.name, err.message);
+						res.status(400);
+						res.setHeader("Cache-Control", "no-store");
+						res.setHeader("Pragma", "no-cache");
 						res.json({ "error": err.message });
 						return;
 					}
 					// send token back
-					res.json({ "uuid": uuid });
+					res.setHeader("Cache-Control", "no-store");
+					res.setHeader("Pragma", "no-cache");
+					res.json({ "access_token": uuid, "token_type": "bearer", "expires_in": 3600 });
 				});
 			});
 		})
 	});
 });
-
-// app.use(express.static(__dirname + '/public'));
 
 const server = http.createServer(app);
 

6c17b4efc1526a013a923e51e120c5ba252f645e
Anton Iun <iunanton@naver.com>
2017-10-12 22:30:19 +0800

    Create guest authentication

--- app.js
+++ app.js
@@ -21,6 +21,48 @@ app.route('/')
 	var grant_type = req.body.grant_type;
 	console.log("%s grant_type: %s", Date.now(), grant_type);
 	switch(grant_type) {
+		case "guest":
+			var username = req.body.username;
+			var code = req.body.code;
+			mongo.connect(url, function(err, db) {
+				db.collection("codes").findOne({ "value": code }, function (err, docs) {
+					if (!docs) {
+						console.log("%s error: invalid_grant", Date.now());
+						res.status(400);
+						res.setHeader("Cache-Control", "no-store");
+						res.setHeader("Pragma", "no-cache");
+						res.json({ "error": "invalid_grant", "error_description": "Code incorrect" });
+						return;
+					}
+					db.collection("users").findOne({ "username": username }, { "isGuest": 1, "isOnline": 1}, function (err, docs) {
+						if (!docs) {
+							var token = uuidv4();
+							var user = { "isGuest": true, "isDeleted": false, "isOnline": false, "username": username, "tokens": [ token ] };
+							db.collection("users").insertOne( user, function(err, r) {
+								res.setHeader("Cache-Control", "no-store");
+								res.setHeader("Pragma", "no-cache");
+								res.json({ "access_token": token, "token_type": "bearer" });
+							});
+						} else {
+							if (docs.isOnline) {
+								console.log("%s error: invalid_grant", Date.now());
+								res.status(400);
+								res.setHeader("Cache-Control", "no-store");
+								res.setHeader("Pragma", "no-cache");
+								res.json({ "error": "invalid_grant", "error_description": "Username is already in use" });
+								return;
+							}
+							var token = uuidv4();
+							db.collection("users").updateOne({ "username": username }, { $set: { "tokens" : [ token ] } }, function (err, r) {
+								res.setHeader("Cache-Control", "no-store");
+								res.setHeader("Pragma", "no-cache");
+								res.json({ "access_token": token, "token_type": "bearer" });
+							});
+						}
+					});
+				});
+			});
+			break;
 		case "password":
 			var username = req.body.username;
 			var password = req.body.password;
